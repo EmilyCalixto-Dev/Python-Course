17dee8d10d8b04a01bb33516a382c80ae8742560
Emily Calixto <213698366+EmilyCalixto-Dev@users.noreply.github.com>
2026-08-17 22:35:59 +0200

    Improve exercise 016

--- Module-1/ex016.py
+++ Module-1/ex016.py
@@ -1,3 +1,3 @@
 import math
 num = float(input('Digite um número: '))
-print(' O número {} tem a parte inteira {}'.format(num, math.floor(num)))
+print(' O número {} tem a parte inteira {}'.format(num, math.trunc(num)))
